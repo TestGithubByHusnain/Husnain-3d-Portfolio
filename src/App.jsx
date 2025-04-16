0b43b2b5568e1dab1ce5d655f2226fc8a7def563
Husnain <husnainali41940@gmail.com>
2025-04-16 03:12:51 -0700

    Developed experience section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import LogoSection from "./sections/LogoSection"
+import NavBar from "./componenets/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
+import ExperienceSection from "./sections/ExperienceSection"
 
 
 const App = () => {
   return (
     <>
+    <NavBar />
     <Hero />
     <ShowcaseSection />
+    <LogoSection />
+    <FeatureCards />
+    <ExperienceSection />
     
     </>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -147,11 +147,11 @@ const navLinks = [
   
   const expCards = [
     {
-      review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-      imgPath: "/images/exp1.png",
-      logoPath: "/images/logo1.png",
-      title: "Frontend Developer",
-      date: "January 2023 - Present",
+      review: "Husnain Ali consistently demonstrated exceptional dedication, professionalism, and a strong work ethic throughout his internship. He actively contributed to key projects, gaining valuable experience and showcasing solid skills in React development. His commitment and performance make him a promising asset in any development team.",
+      imgPath: "/images/codexcue.png",
+     logoPath: "/images/codexcue.png",
+      title: "React Developer (Intern)",
+      date: "1st November 2024 to 30th November 2024",
       responsibilities: [
         "Developed and maintained user-facing features for the Hostinger website.",
         "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
@@ -159,11 +159,11 @@ const navLinks = [
       ],
     },
     {
-      review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-      imgPath: "/images/exp2.png",
-      logoPath: "/images/logo2.png",
-      title: "Full Stack Developer",
-      date: "June 2020 - December 2023",
+      review: "Husnain delivered impressive performance during his internship, making valuable contributions to our team. He demonstrated strong analytical skills, quickly adapted to new technologies, and maintained high productivity. His collaborative nature and willingness to help others stood out. We confidently recommend him as a dedicated and capable professional for any future opportunity.",
+      imgPath: "/images/codealpha.png",
+      logoPath: "/images/codealpha.png",
+      title: "Front-End Web Developer (Intern)",
+      date: "1st October 2024 to 30th October 2024",
       responsibilities: [
         "Led the development of Docker's web applications, focusing on scalability.",
         "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
@@ -171,11 +171,11 @@ const navLinks = [
       ],
     },
     {
-      review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-      imgPath: "/images/exp3.png",
-      logoPath: "/images/logo3.png",
-      title: "React Native Developer",
-      date: "March 2019 - May 2020",
+      review: "Husnain made excellent use of DevelopersHub to stay organized and efficient. He managed projects, tracked tasks, and maintained clear documentation with consistency. His structured approach and attention to detail made him a reliable and proactive team member.",
+      imgPath: "/images/developershub.png",
+      logoPath:"/images/developershub.png",
+      title: "Front-End Web Developer (Intern)",
+      date: "1st September 2024 to 15th October 2024",
       responsibilities: [
         "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
         "Improved app performance and user experience through code optimization and testing.",
